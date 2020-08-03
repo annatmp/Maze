1160fbcc2a6ff964ddd95dcb7c75765e4be5376f
anna <apillar3@gmail.com>
2020-08-03 20:59:39 +0200

    added a option selection screen and a controller

--- view.py
+++ view.py
@@ -3,8 +3,6 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from maze import Maze
 
-import sys
-
 # Colour palette
 MAIN_BACKGROUND = QColor("#F2E9E4")
 PURPLE = QColor("#713E5A")
@@ -12,39 +10,59 @@ TURQUOISE = QColor("#2292A4")
 DARK_GREY = QColor("#2A2C24")
 
 
+class Controller(QObject):
+
+    def __init__(self):
+        super(Controller, self).__init__()
+        self.main = View()
+        self.load_main_screen()
+
+    def load_main_screen(self):
+        landing_screen = LandingScreen(self.main, self)
+        self.main.change_screen(landing_screen)
+
+    def generate_maze(self):
+        generation_mode = self.option_screen.get_selected_generation()
+        solving_strategy = self.option_screen.get_selected_solving()
+        height, width = self.option_screen.get_selected_size()
+
+        maze = Maze(height, width, generation_mode, solving_strategy)
+
+        main_game_screen = MainGameScreen(self.main, maze, self)
+        self.main.change_screen(main_game_screen)
+
+    def continue_after_landing_screen(self):
+        self.option_screen = MazePropertiesSelection(self.main, self)
+        self.main.change_screen(self.option_screen)
+
+    def start_solving(self):
+        pass
+
+
 class View(QMainWindow):
 
-    def __init__(self, maze):
+    def __init__(self):
         super(View, self).__init__()
 
         self.minimum_size = QSize(650, 700)
         self.setWindowTitle("A-Maze-ing")
         self.setWindowIcon(QIcon('assets/maze.png'))
 
-        self.maze = maze
-        landing_screen = Landing_Screen(self)
-
-        self.setCentralWidget(landing_screen)
         self.setMinimumSize(self.minimum_size)
         self.show()
 
-    def load_maze(self):
-        main_game_screen = Main_Game_Screen(self, self.maze)
-        self.setCentralWidget(main_game_screen)
-        self.update()
-
-    def load_main_screen(self):
-        landing_screen = Landing_Screen(self)
-        self.setCentralWidget(landing_screen)
+    def change_screen(self, screen: QWidget):
+        self.setCentralWidget(screen)
         self.update()
 
 
-class Landing_Screen(QWidget):
+class LandingScreen(QWidget):
 
-    def __init__(self, parent):
-        super(Landing_Screen, self).__init__(parent)
+    def __init__(self, parent, controller: Controller):
+        super(LandingScreen, self).__init__(parent)
 
         self.parent = parent
+        self.controller = controller
         self.layout = QVBoxLayout(self)
 
         # Title
@@ -61,7 +79,7 @@ class Landing_Screen(QWidget):
         # start button
         self.start_button = QPushButton(self)
         self.start_button.setText("Start")
-        self.start_button.clicked.connect(self.parent.load_maze)
+        self.start_button.clicked.connect(self.controller.continue_after_landing_screen)
         self.layout.addWidget(self.start_button, alignment=Qt.AlignCenter)
 
         self.setLayout(self.layout)
@@ -71,40 +89,39 @@ class Landing_Screen(QWidget):
     def colour_styling(self):
         self.setAutoFillBackground(True)
         palette = self.palette()
-        palette.setColor(QPalette.Window,MAIN_BACKGROUND)
+        palette.setColor(QPalette.Window, MAIN_BACKGROUND)
         self.setPalette(palette)
 
 
-class Main_Game_Screen(QWidget):
+class MainGameScreen(QWidget):
 
-    def __init__(self, parent: QMainWindow, maze):
+    def __init__(self, parent: QMainWindow, maze, controller: Controller):
         """
 
         :type parent: QMainWindow
         """
-        super(Main_Game_Screen, self).__init__(parent)
+        super(MainGameScreen, self).__init__(parent)
         self.maze = maze
         self.parent = parent
-
+        self.controller = controller
         self.min_size = QSize(600, 600)
 
-        #Generate children widgets'
+        # Generate children widgets'
 
-        self.maze_canvas = Maze_Canvas(self, maze)
+        self.maze_canvas = MazeCanvas(self, maze)
 
         # Return home button
-        self.return_to_home = QPushButton("Return to Homescreen")
+        self.return_to_home = QPushButton("Return to home screen")
         self.return_to_home.adjustSize()
-        self.return_to_home.clicked.connect(self.parent.load_main_screen)
+        self.return_to_home.clicked.connect(self.controller.load_main_screen)
 
-        self.info_header = Maze_Screen_Header(self, maze.get_mode_as_nice_string(), maze.get_path_length(), False)
+        self.info_header = MazeScreenHeader(self, maze.get_mode_as_nice_string(), maze.get_path_length(), False)
 
         self.layout = QGridLayout()
         self.layout.addWidget(self.info_header, 0, 0, 1, 3)
         self.layout.addWidget(self.maze_canvas, 1, 0, 1, 3, Qt.AlignCenter)
         self.layout.addWidget(self.return_to_home, 2, 1, Qt.AlignCenter)
         self.setLayout(self.layout)
-
 
         self.colour_styling()
         self.adjustSize()
@@ -114,17 +131,16 @@ class Main_Game_Screen(QWidget):
     def colour_styling(self):
         self.setAutoFillBackground(True)
         palette = self.palette()
-        palette.setColor(QPalette.Window,MAIN_BACKGROUND)
+        palette.setColor(QPalette.Window, MAIN_BACKGROUND)
         self.setPalette(palette)
 
     def resizeEvent(self, a0: QResizeEvent) -> None:
-
         self.layout.setAlignment(self.info_header, Qt.AlignCenter)
-        self.setMinimumSize(int(self.parent.size().width()*0.8), int(self.parent.size().height()*0.8))
+        self.setMinimumSize(int(self.parent.size().width() * 0.8), int(self.parent.size().height() * 0.8))
         self.maze_canvas.resizeEvent(a0)
 
 
-class Maze_Screen_Header(QWidget):
+class MazeScreenHeader(QWidget):
     """
     Header to be displayed above the maze. Gives basic information about the generated maze
     Info is pulled from maze directly
@@ -139,17 +155,16 @@ class Maze_Screen_Header(QWidget):
         :param search_mode: used to generate maze
         """
 
-        super(Maze_Screen_Header, self).__init__(parent)
+        super(MazeScreenHeader, self).__init__(parent)
         self.parent = parent
 
         self.generation_mode = QLabel(generation_mode)
 
         if search_mode:
             self.search_mode = QLabel(search_mode)
-        #Default
+        # Default
         else:
             self.search_mode = QLabel("No search mode selected")
-
 
         self.path_length = QLabel("Path Length: {}".format(path_length))
 
@@ -164,18 +179,107 @@ class Maze_Screen_Header(QWidget):
 
     def resizeEvent(self, a0: QResizeEvent) -> None:
         self.layout.setAlignment(self.search_mode, Qt.AlignCenter)
-        self.setMinimumWidth(int(self.parent.size().width()*0.8))
+        self.setMinimumWidth(int(self.parent.size().width() * 0.8))
 
 
-class Maze_Canvas(QLabel):
+class MazePropertiesSelection(QWidget):
+
+    def __init__(self, parent: QWidget, controller: Controller):
+        super(MazePropertiesSelection, self).__init__(parent)
+        self.parent = parent
+        self.controller = controller
+        self.no_solving_option_string = "No Solving, just generate"
+
+        generation_options = Maze.GENERATION_ALGORITHMS
+        solving_options = Maze.SOLVING_ALGORITHMS
+        solving_options.append(self.no_solving_option_string)
+
+        self.generation_dropdown = OptionDropdown(self, generation_options)
+        self.solving_dropdown = OptionDropdown(self, solving_options)
+
+        self.height_selector = QSpinBox()
+        self.width_selector = QSpinBox()
+
+        for size_selector in [self.height_selector, self.width_selector]:
+            size_selector.setMinimum(5)
+            size_selector.setMaximum(Maze.SIZE_LIMIT)
+
+        choose_generation_label = QLabel("Please choose an algorithm for generating the maze.")
+        choose_solving_label = QLabel("Please choose an algorithm for solving the maze. Choose None if you "
+                                      "only want to generate a maze")
+
+        choose_size_label = QLabel("Please choose a width and height for the maze.")
+        width_label = QLabel("Width:")
+        height_label = QLabel("Height:")
+
+        for label in [choose_generation_label, choose_solving_label, choose_size_label]:
+            label.setWordWrap(True)
+
+        layout = QGridLayout()
+
+        size_selection_layout = QGridLayout()
+        size_selection_layout.addWidget(choose_size_label, 0, 0, 1, 2, Qt.AlignCenter)
+        size_selection_layout.addWidget(width_label, 1, 0)
+        size_selection_layout.addWidget(self.width_selector, 2, 0)
+        size_selection_layout.addWidget(height_label, 1, 1)
+        size_selection_layout.addWidget(self.height_selector, 2, 1)
+
+        generation_layout = QVBoxLayout()
+        generation_layout.addWidget(choose_generation_label)
+        generation_layout.addWidget(self.generation_dropdown)
+
+        solving_layout = QVBoxLayout()
+        solving_layout.addWidget(choose_solving_label)
+        solving_layout.addWidget(self.solving_dropdown)
+
+        confirm_button = QPushButton("Start")
+        confirm_button.clicked.connect(self.controller.generate_maze)
+
+        layout.addItem(generation_layout, 0, 0)
+        layout.addItem(solving_layout, 0, 1)
+        layout.addItem(size_selection_layout, 1, 0, 1, 2)
+        layout.addWidget(confirm_button, 2, 2, Qt.AlignLeft)
+
+        self.setLayout(layout)
+
+    def get_selected_generation(self):
+
+        return self.generation_dropdown.currentText()
+
+    def get_selected_solving(self):
+
+        selected_solving = self.solving_dropdown.currentText()
+
+        if selected_solving == self.no_solving_option_string:
+            selected_solving = False
+
+        return selected_solving
+
+    def get_selected_size(self):
+        return self.height_selector.value(), self.width_selector.value()
+
+
+class OptionDropdown(QComboBox):
+
+    def __init__(self, parent, options):
+        super(QComboBox, self).__init__(parent)
+        self.parent = parent
+
+        self.addItems(options)
+
+    def styling(self):
+        pass
+
+
+class MazeCanvas(QLabel):
 
     def __init__(self, parent, maze):
-        super(Maze_Canvas, self).__init__(parent)
+        super(MazeCanvas, self).__init__(parent)
 
         self.parent = parent
         self.THICKNESS = 3
 
-        self.scale_factor = 20 # once cell equals 20 pixel
+        self.scale_factor = 20  # once cell equals 20 pixel
         self.scale = lambda x: x * self.scale_factor
 
         self.maze = maze
@@ -258,12 +362,3 @@ class Maze_Canvas(QLabel):
         end = QPixmap("assets/target.png")
 
         painter.drawPixmap(rect, end)
-
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    maze = Maze(30, 30, "depth_first")
-    GUI = View(maze)
-
-    sys.exit(app.exec_())
